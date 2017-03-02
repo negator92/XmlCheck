0fc5250f3e6fcacae4bcb20638e58611320f20f6
Vincent <negator92@gmail.com>
2017-03-02 10:32:11 +0300

    First commit

---
+++ Tag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Tag
+{
+    public static int PayCaseCounter;
+    public static int PayCaseNumber;
+
+    public static void CallPrg(string[] args)
+    {
+        //Checking for arguments
+        if (args.Length == 0)
+            Console.WriteLine("Использование: Check.exe file.xml");
+        else if (args.Length == 1)
+        {
+            Console.WriteLine("1 arg");
+        }
+    }
+}
