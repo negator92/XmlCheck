a0ed4ff59e20d4eea02f9d647ed57403d7a43188
Vincent <negator92@gmail.com>
2017-03-06 15:01:44 +0300

    Correct Directory.GetFiles

--- Tag.cs
+++ Tag.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text.RegularExpressions;
 using System.Xml;
 
@@ -7,8 +9,7 @@ public class Tag
 {
     public static int PayCaseCounter;
     public static int PayCaseNumber;
-    Regex regex = new Regex(@"^([А-ЯЁ]+)([\s\-]?[А-ЯЁ]+)*$");
-    public static string[] array;
+    //public static string[] array;
 
     public static void CallPrg(string[] args)
     {
@@ -17,13 +18,13 @@ public class Tag
             Console.WriteLine("Использование: XmlCheck.exe file.xml");
         else
         {
-            foreach (string arg1 in args)
+            foreach (string arg in args)
             {
-                string[] array = Directory.GetFiles(".", arg1);
+                string[] array = (args.SelectMany(arg1 => Directory.GetFiles(".", arg)).Distinct()).ToArray();
                 for (int i = 0; i < array.Length; i++)
-                {
-                    Console.WriteLine("Argument {0} link to {1} file.", arg1, array[i]);
-                }
+                    {
+                        Console.WriteLine("Argument {0} link to {1} file.", arg, array[i]);
+                    }
             }
         }
     }
@@ -54,63 +55,63 @@ public class Tag
     public static void CheckingXmlReader(string xmlFile)
     {
         using (XmlReader reader = XmlReader.Create(xmlFile))
-        {
-            Console.WriteLine(xmlFile);
-            PayCaseCounter = 0;
-            //Open stream with "read" status
-            while (reader.Read())
-            {
-                //Regular expr
-                Regex regex = new Regex(@"^([А-ЯЁ]+)([\s\-]?[А-ЯЁ]+)*$");
-                switch (reader.Name)
-                {
-                    case "НомерВыплатногоДела":
-                        PayCaseNumber = reader.ReadElementContentAsInt();
-                        PayCaseCounter++;
-                        break;
-                    case "Фамилия":
-                        string secondName = reader.ReadElementContentAsString();
-                        if (regex.IsMatch(secondName))
-                            break;
-                        else
+                    {
+                        Console.WriteLine(xmlFile);
+                        PayCaseCounter = 0;
+                        //Open stream with "read" status
+                        while (reader.Read())
                         {
-                            FileStream file = new FileStream(xmlFile + ".log", FileMode.Append);
-                            StreamWriter writer = new StreamWriter(file);
-                            writer.Write("{0}\n", PayCaseNumber);
-                            writer.Close();
-                            Console.WriteLine(secondName);
-                            break;
+                            //Regular expr
+                            Regex regex = new Regex(@"^([А-ЯЁ]+)([\s\-]?[А-ЯЁ]+)*$");
+                            switch (reader.Name)
+                            {
+                                case "НомерВыплатногоДела":
+                                    PayCaseNumber = reader.ReadElementContentAsInt();
+                                    PayCaseCounter++;
+                                    break;
+                                case "Фамилия":
+                                    string secondName = reader.ReadElementContentAsString();
+                                    if (regex.IsMatch(secondName))
+                                        break;
+                                    else
+                                    {
+                                        FileStream file = new FileStream(xmlFile + ".log", FileMode.Append);
+                                        StreamWriter writer = new StreamWriter(file);
+                                        writer.Write("{0}\n", PayCaseNumber);
+                                        writer.Close();
+                                        Console.WriteLine(secondName);
+                                        break;
+                                    }
+                                case "Имя":
+                                    string firstName = reader.ReadElementContentAsString();
+                                    if (regex.IsMatch(firstName))
+                                        break;
+                                    else
+                                    {
+                                        FileStream file = new FileStream(xmlFile + ".log", FileMode.Append);
+                                        StreamWriter writer = new StreamWriter(file);
+                                        writer.Write("{0}\n", PayCaseNumber);
+                                        writer.Close();
+                                        Console.WriteLine(firstName);
+                                        break;
+                                    }
+                                case "Отчество":
+                                    string middleName = reader.ReadElementContentAsString();
+                                    if (middleName.Length == 0 || regex.IsMatch(middleName))
+                                        break;
+                                    else
+                                    {
+                                        FileStream file = new FileStream(xmlFile + ".log", FileMode.Append);
+                                        StreamWriter writer = new StreamWriter(file);
+                                        writer.Write("{0}\n", PayCaseNumber);
+                                        writer.Close();
+                                        Console.WriteLine(middleName);
+                                        break;
+                                    }
+                            }
                         }
-                    case "Имя":
-                        string firstName = reader.ReadElementContentAsString();
-                        if (regex.IsMatch(firstName))
-                            break;
-                        else
-                        {
-                            FileStream file = new FileStream(xmlFile + ".log", FileMode.Append);
-                            StreamWriter writer = new StreamWriter(file);
-                            writer.Write("{0}\n", PayCaseNumber);
-                            writer.Close();
-                            Console.WriteLine(firstName);
-                            break;
-                        }
-                    case "Отчество":
-                        string middleName = reader.ReadElementContentAsString();
-                        if (middleName.Length == 0 || regex.IsMatch(middleName))
-                            break;
-                        else
-                        {
-                            FileStream file = new FileStream(xmlFile + ".log", FileMode.Append);
-                            StreamWriter writer = new StreamWriter(file);
-                            writer.Write("{0}\n", PayCaseNumber);
-                            writer.Close();
-                            Console.WriteLine(middleName);
-                            break;
-                        }
-                }
-            }
-            Console.WriteLine(PayCaseCounter);
-            reader.Close();
-        }
+                        Console.WriteLine(PayCaseCounter);
+                        reader.Close();
+                    }
     }
 }
