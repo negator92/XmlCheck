b398f1f9cc8d74bd85ef7f5ba31b5b2b8cd2de73
Vincent Neparidze <nva@akmetron.ru>
2017-12-17 09:14:17 +0300

    Overwritten with OOP style

---
+++ XmlCheck/FileXml.cs
@@ -0,0 +1,87 @@
+﻿#define DEMO
+using System;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Xml;
+
+namespace XmlCheck
+{
+    public class FileXml
+    {
+        public FileXml(string FileName)
+        {
+            this.FileName = FileName;
+        }
+
+        public static int payCaseCounter = 0;
+        public static Regex regex = new Regex(@"^([А-ЯЁ]+)([\s\-]?[А-ЯЁ]+)*$", RegexOptions.Compiled);
+
+        public int payCaseNumber;
+        public string FileName { get; set; }
+
+        public string Check()
+        {
+#if DEMO
+            var watch = System.Diagnostics.Stopwatch.StartNew();
+#endif
+            using (XmlReader xmlReader = XmlReader.Create(FileName))
+            {
+                while (xmlReader.Read())
+                {
+                    switch (xmlReader.Name)
+                    {
+                        case "НомерВыплатногоДела":
+                            payCaseNumber = xmlReader.ReadElementContentAsInt();
+                            payCaseCounter++;
+                            break;
+                        case "Фамилия":
+                            string secondName = xmlReader.ReadElementContentAsString();
+                            if (regex.IsMatch(secondName))
+                                break;
+                            else
+                            {
+                                FileStream file = new FileStream(FileName + ".log", FileMode.Append);
+                                StreamWriter writer = new StreamWriter(file);
+                                writer.Write("{0}\n", payCaseNumber);
+                                writer.Close();
+                                Console.WriteLine(secondName);
+                                break;
+                            }
+                        case "Имя":
+                            string firstName = xmlReader.ReadElementContentAsString();
+                            if (regex.IsMatch(firstName))
+                                break;
+                            else
+                            {
+                                FileStream file = new FileStream(FileName + ".log", FileMode.Append);
+                                StreamWriter writer = new StreamWriter(file);
+                                writer.Write("{0}\n", payCaseNumber);
+                                writer.Close();
+                                Console.WriteLine(firstName);
+                                break;
+                            }
+                        case "Отчество":
+                            string middleName = xmlReader.ReadElementContentAsString();
+                            if (middleName.Length == 0 || regex.IsMatch(middleName))
+                                break;
+                            else
+                            {
+                                FileStream file = new FileStream(FileName + ".log", FileMode.Append);
+                                StreamWriter writer = new StreamWriter(file);
+                                writer.Write("{0}\n", payCaseNumber);
+                                writer.Close();
+                                Console.WriteLine(middleName);
+                                break;
+                            }
+                    }
+                }
+                xmlReader.Close();
+#if DEMO
+                watch.Stop();
+                Console.WriteLine("Checking {0} takes {1} miliseconds", FileName, watch.ElapsedMilliseconds);
+#endif
+            }
+            return $"Проверено {payCaseCounter} дел в файле {FileName}.";
+        }
+    }
+}
--- XmlCheck/Program.cs
+++ XmlCheck/Program.cs
@@ -1,16 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
 
 namespace XmlCheck
 {
     class Program
     {
+        public static string xmlHead = "<?xml version=\"1.0\" encoding=\"WINDOWS-1251\"?>";
+
         static void Main(string[] args)
         {
-            //All actions should be called from special class
-            Tag.CallPrg(args);
+            List<string> filesNames = new List<string>();
+
+            if (args.Length == 0)
+                Console.WriteLine("Использование: XmlCheck.exe file.xml");
+
+            foreach (string fileName in args)
+            {
+                if (!File.Exists(fileName))
+                    Console.WriteLine($"Файл {fileName} не найден");
+                else if (xmlHead != File.ReadLines(fileName).First())
+                    Console.WriteLine($"{fileName} не XML файл");
+                else
+                    filesNames.Add(fileName);
+            }
+
+            FileXml[] filesArray = new FileXml[filesNames.Count];
+            int i = 0;
+            foreach (var fileName in filesNames)
+            {
+                filesArray[i] = new FileXml(fileName);
+                i++;
+            }
+
+            foreach (FileXml fileXml in filesArray)
+                fileXml.Check();
         }
     }
 }
